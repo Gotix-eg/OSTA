d84b434c5aae11f081efbcc391bd249221941322
Hussein <hgomaa0@gmail.com>
2026-04-07 23:40:13 +0200

    fix(prisma): use unpooled string

--- apps/server/src/lib/prisma.ts
+++ apps/server/src/lib/prisma.ts
@@ -4,7 +4,13 @@ declare global {
   var __ostaPrisma__: PrismaClient | undefined;
 }
 
-export const prisma = globalThis.__ostaPrisma__ ?? new PrismaClient();
+export const prisma = globalThis.__ostaPrisma__ ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL_UNPOOLED || process.env.DATABASE_URL
+    }
+  }
+});
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.__ostaPrisma__ = prisma;
