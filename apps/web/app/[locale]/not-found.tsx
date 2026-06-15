370fba6d75b49cfb72f0a05684a247f7200dfe36
Hussein <hgomaa0@gmail.com>
2026-06-15 15:18:11 +0300

    feat(web): integrate branded localized 404 page handler and catch-all routes

---
+++ apps/web/app/[locale]/not-found.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { NotFoundView } from "@/components/public/not-found-view";
+
+export default function LocaleNotFound() {
+  return <NotFoundView />;
+}
---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { NotFoundView } from "@/components/public/not-found-view";
+
+export default function RootNotFound() {
+  return <NotFoundView />;
+}
